9f2e22880635c4668e8fc3bcec31bbbe3e7e4297
Chase McCarthy <dev+code0100fun@hashrocket.com>
2015-04-19 02:50:59 -0400

    Add episode 5

--- app/models/episodes.js
+++ app/models/episodes.js
@@ -1,5 +1,18 @@
 export default [
   {
+    number: 5,
+    title: "Wicked Good Design",
+    description: "Chase and Jonathan discuss the site redesign, Wicked Good Ember, Ember in Viewport, security patches, the run loop, and Ember Islands.",
+    slug: "wicked-good-design",
+    releaseDate: "April 20, 2015",
+    lat: 30.291809,
+    long: -81.390607,
+    filename: "ep-5-ember-weekend",
+    author: "Chase McCarthy & Jonathan Jackson",
+    image: "https://i.imgur.com/XV0c9Xl.png",
+    duration: "13:40"
+  },
+  {
     number: 4,
     title: "IE: Seeya L8er",
     description: "Chase and Jonathan talk Polymer, Ember 2.0 training, Netflix's freedom of responsiblity, and spend some extra time talking about IE8 support.",
