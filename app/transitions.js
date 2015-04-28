37b1aa1260f5648852beaa3839cd6a743b048104
Chase McCarthy <dev+code0100fun@hashrocket.com>
2015-04-27 22:01:13 -0400

    Add animations between routes

---
+++ app/transitions.js
@@ -0,0 +1,12 @@
+export default function(){
+  this.transition(
+    this.fromRoute('episodes.index'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+  this.transition(
+    this.toRoute('root.about'),
+    this.use('toUp'),
+    this.reverse('toDown')
+  );
+}
