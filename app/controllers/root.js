5e6f379e6482a1f4cb649e43c5299594ea96c18b
Chase McCarthy <dev+code0100fun@hashrocket.com>
2015-05-07 18:49:52 -0400

    Remove old menu code from root controller

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -1,10 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  showMenu: false,
-  actions: {
-    toggleMenu: function(){
-      this.toggleProperty('showMenu');
-    }
-  }
-});
+export default Ember.Controller.extend({});
