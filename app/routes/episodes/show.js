421eeb9752371142dde31eb4a304b080aa258425
Chase McCarthy <dev+code0100fun@hashrocket.com>
2015-05-07 19:24:16 -0400

    Render shared episode show template

--- app/routes/episodes/show.js
+++ app/routes/episodes/show.js
@@ -5,8 +5,9 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.get('episodes').find(params.slug);
   },
-  renderTemplate: function(controller, model) {
-    this.render(`episodes/${model.get('slug')}`, { view: 'episodes/show'});
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    controller.set('episodeTemplate', `episodes/${model.get('slug')}`);
   },
   serialize: function(model) {
     return { slug: model.get('slug') };
