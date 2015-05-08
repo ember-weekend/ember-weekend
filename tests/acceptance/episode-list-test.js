4bb423ea50602b15336aa3be5965f0180b546c6a
Jonathan Jackson <jonathan.jackson1@me.com>
2015-05-08 14:51:40 -0400

    Add simple acceptance test for episodes index
    This seems super complicated because of the way we have to stub out the
    episode service in an acceptance test.
    
    For more informationlease refer to:
    
    http://stackoverflow.com/questions/28422375/how-to-mock-an-ember-cli-service-in-an-acceptance-test
    
    Additional sources / implementations:
    
    https://github.com/stefanpenner/ember-jobs/commit/5b027e78a3980f14067d6237e41e86994fe9019a#diff-9c6c3a9ea701f78a2f4140aa824db351
    https://github.com/ember-cli/ember-cli/pull/3306/files
    https://github.com/alexlafroscia/ember-cli/commit/ceac39be1cda74a884b2a6fcb181fb84b1514e6d
    
    /ht SaladFork!

---
+++ tests/acceptance/episode-list-test.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import {
+  module,
+  test
+} from 'qunit';
+import startApp from 'ember-weekend/tests/helpers/start-app';
+import { stubResolver } from '../helpers/container';
+
+var application;
+
+var episodes = [
+  { number: 2,
+    title: "Quux title",
+    description: "Bar",
+    slug: "Baz",
+    releaseDate: "May 1, 2015",
+    author: "Tomster"
+  },
+  { number: 1,
+    title: "Foo title",
+    description: "Bar",
+    slug: "Baz",
+    releaseDate: "May 8, 2015",
+    author: "Tomster"
+  }
+];
+
+var episodesProxy = Ember.ArrayProxy.create({
+  content: episodes.map(function(episode){
+    return Ember.Object.create(episode);
+  })
+});
+
+var mockEpisodeService = Ember.Service.extend({
+  all: function(){ return episodesProxy; },
+  mostRecent: function(){ return this.all().objectAt(0); }
+});
+
+module('Acceptance: EpisodeList', {
+  beforeEach: function() {
+    application = startApp({}, function(app) {
+      stubResolver(app, 'service:episodes', mockEpisodeService);
+    });
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /episodes', function(assert) {
+  visit('/episodes');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/episodes');
+    assert.equal(find('ul.episodes li:last h1').text(), 'Foo title');
+    assert.equal(find('ul.episodes li:first h1').text(), 'Quux title');
+  });
+});
