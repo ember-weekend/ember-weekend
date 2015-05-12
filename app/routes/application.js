795e4cad0ad8fcfbfa306a7de98770a8add68d29
Jonathan Jackson <jonathan.jackson1@me.com>
2015-05-12 16:15:40 -0500

    Add space bar play/pause toggle
    [finishes #9]
    https://github.com/ember-weekend/ember-weekend/issues/9
    
    Utilizing Mousetrap in order to handle click events.  More information
    on that and associated ember addon:
    
    Mousetrap: https://github.com/ccampbell/mousetrap
    Ember CLI Mousetrap addon: https://github.com/idobata/ember-mousetrap
    
    In order to test this thoroughly I've included the KeyEvent test helper
    from Moustrap's repository.  Review here:
    
    https://github.com/ccampbell/mousetrap/blob/master/tests/libs/key-event.js
    
    This does an excellent job at allow us to simulate actual keyboard
    events reliably.  Other techniques I tried seemed pretty lacking.
    
    This will undoubtedly be useful as we add more keyboard shortcuts.

---
+++ app/routes/application.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import { MousetrapRoute, mousetrap } from 'ember-mousetrap';
+
+export default Ember.Route.extend(MousetrapRoute, {
+  player: Ember.inject.service('player'),
+  shortcuts: {
+    togglePlay: mousetrap('space', function(){
+      var player = this.get('player');
+      if(player.get('playing'))  {
+        player.pause();
+      } else {
+        player.play();
+      }
+    })
+  }
+});
---
+++ tests/acceptance/keyboard-shortcuts-test.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+import {
+  module,
+  test
+} from 'qunit';
+import startApp from 'ember-weekend/tests/helpers/start-app';
+import { stubResolver } from '../helpers/container';
+import mockKeyEvent from 'ember-weekend/utils/mock-key-events';
+
+var application;
+
+module('Acceptance: KeyboardShortcuts', {
+  beforeEach: function(assert) {
+    window.mockKeyEvent = mockKeyEvent;
+
+    var mockPlayerService = Ember.Service.extend({
+      play: function(){
+        assert.ok(true);
+      },
+      pause: function(){
+        assert.ok(true);
+      }
+    });
+
+    application = startApp({}, function(app) {
+      stubResolver(app, 'service:player', mockPlayerService);
+    });
+  },
+
+  afterEach: function() {
+    window.mockKeyEvent = undefined;
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /keyboard-shortcuts', function(assert) {
+  visit('/episodes');
+  assert.expect(2);
+
+  andThen(function() {
+    mockKeyEvent.simulate(32,32); // play()
+    mockKeyEvent.simulate(32,32); // pause()
+  });
+});
