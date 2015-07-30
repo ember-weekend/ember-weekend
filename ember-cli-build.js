0354b6f6575483124c445ca423aeaec81c4c8f91
Jonathan Jackson <jonathan.jackson1@me.com>
2015-07-30 17:42:46 -0400

    Update to ember-cli 1.13.6 and ember 1.13.5

---
+++ ember-cli-build.js
@@ -0,0 +1,33 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  app.import(app.bowerDirectory + '/moment/moment.js');
+  app.import(app.bowerDirectory + '/ember-cli-moment-shim/moment-shim.js', {
+    exports: {
+      moment: ['default']
+    }
+  });
+
+  var fs = require('fs');
+  var appTree;
+
+  if(fs.existsSync('media')){
+    var mergeTrees = require('broccoli-merge-trees');
+    var RSSGenerator = require('./rss-generator');
+    var rss = new RSSGenerator({
+      feed: './app/models/show.js',
+      items: './app/models/episodes.js',
+      output: 'feed.xml'
+    });
+    appTree = mergeTrees([app.toTree(), rss]);
+  }else{
+    appTree = app.toTree();
+  }
+
+  return appTree;
+};
