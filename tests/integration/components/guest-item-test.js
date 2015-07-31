bf07f907e38f527781cec83ca589840af59d5a12
Jonathan Jackson <jonathan.jackson1@me.com>
2015-07-31 16:31:01 -0400

    Add guest-ui

---
+++ tests/integration/components/guest-item-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('guest-item', 'Integration | Component | guest item', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{guest-item
+                    name='Flirgity Schmerga'
+                    guestInfoURL='http://doom.com'
+                    avatarURL='http://example.com'
+                    tagLine='Javascript Developer at Blerga Inc.'
+                    bio="I'm a cool person, yo!"
+                  }}`);
+
+  assert.trimEq(this.$().find('li.guest img').attr('src'), 'http://example.com');
+  assert.equal(this.$().find('.guest_bio h1 a').text(), 'Flirgity Schmerga');
+  assert.equal(this.$().find('.guest_bio h1 a').attr('href'), 'http://doom.com');
+  assert.equal(this.$().find('.guest_bio p:first').text(), 'Javascript Developer at Blerga Inc.');
+  assert.equal(this.$().find('.guest_bio p.bio').text(), "I'm a cool person, yo!");
+});
