65f8dee05e889cd975877d2ab5601b6b014dca2f
Jonathan Jackson <jonathan.jackson1@me.com>
2015-09-26 00:13:23 -0400

    Finish show-notes for episode 001-our-first-foray

--- app/models/show-notes.js
+++ app/models/show-notes.js
@@ -26,5 +26,69 @@ export default [
         title: 'Chase McCarthy'
       }
     ]
+  },
+  {
+    episode: 1,
+    timeStamp: '2:41',
+    resource: {
+      link: 'https://babeljs.io/docs/learn-es6/',
+      title: 'Babel: Learn ES6'
+    },
+    authors: [
+      {
+        link: 'https://github.com/sebmck',
+        title: 'Sebastian McKenzie'
+      }
+    ]
+  },
+  {
+    episode: 1,
+    timeStamp: '03:58',
+    resource: {
+      link: 'http://emberobserver.com',
+      title: 'Ember Observer'
+    },
+    authors: [
+      {
+        link: 'http://www.codeallday.com',
+        title: 'Katie Gengler'
+      },
+      {
+        link: 'http://www.louissimons.com',
+        title: 'Louis Simons'
+      },
+      {
+        link: 'http://www.pgengler.net',
+        title: 'Phil Gengler'
+      }
+    ]
+  },
+  {
+    episode: 1,
+    timeStamp: '05:41',
+    resource: {
+      link: 'https://github.com/rondale-sc/introductory-ember-resources',
+      title: 'Introductory Ember Resource'
+    },
+    authors: [
+      {
+        link: 'https://twitter.com/rondale_sc',
+        title: 'Jonathan Jackson'
+      }
+    ]
+  },
+  {
+    episode: 1,
+    timeStamp: '07:55',
+    resource: {
+      link: 'https://www.youtube.com/watch?v=xsG0gDkvDPw',
+      title: 'Growing Ember: One Tomster at a Time'
+    },
+    authors: [
+      {
+        link: 'https://github.com/jgwhite',
+        title: 'Jamie White'
+      }
+    ]
   }
 ];
