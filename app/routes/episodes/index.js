022aad6007e1ffde16ef669f64d93953f5e36cbd
Jonathan Jackson <dev+rondale-sc@hashrocket.com>
2015-09-27 19:55:24 -0400

    Sort episodes in descending order

--- app/routes/episodes/index.js
+++ app/routes/episodes/index.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   episodes: Ember.inject.service('episodes'),
   model() {
-    return this.store.findAll('episode');
+    return this.store.findAll('episode').then((episodes) => {
+      return episodes.sortBy('number').reverse();
+    });
   }
 });
