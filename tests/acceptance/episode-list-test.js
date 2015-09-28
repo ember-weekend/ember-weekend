64bb31274e14f07575ad687e48fec8978916c9f0
Jonathan Jackson <dev+rondale-sc@hashrocket.com>
2015-09-27 22:12:49 -0400

    Utilize ember-cli-mirage in episode-list acceptance test

--- tests/acceptance/episode-list-test.js
+++ tests/acceptance/episode-list-test.js
@@ -4,7 +4,6 @@ import {
   test
 } from 'qunit';
 import startApp from 'ember-weekend/tests/helpers/start-app';
-import { stubResolver } from '../helpers/container';
 import page from './pages/episodes-page';
 
 let application;
@@ -26,26 +25,9 @@ const episodes = [
   }
 ];
 
-const episodesProxy = Ember.ArrayProxy.create({
-  content: episodes.map(function(episode) {
-    return Ember.Object.create(episode);
-  })
-});
-
-const mockEpisodeService = Ember.Service.extend({
-  findAll() {
-    return episodesProxy;
-  },
-  queryRecord() {
-    return this.all().objectAt(0);
-  }
-});
-
 module('Acceptance: EpisodeList', {
   beforeEach() {
-    application = startApp({}, function(app) {
-      stubResolver(app, 'service:store', mockEpisodeService);
-    });
+    application = startApp();
   },
 
   afterEach() {
@@ -54,6 +36,10 @@ module('Acceptance: EpisodeList', {
 });
 
 test('visiting /episodes', function(assert) {
+  episodes.forEach(function(e) {
+    server.create('episode', e);
+  });
+
   page.visit();
 
   andThen(function() {
