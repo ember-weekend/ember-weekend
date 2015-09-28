7e7c8090554d606ac41bf6bfd6c7f9b13c507940
Jonathan Jackson <dev+rondale-sc@hashrocket.com>
2015-09-28 00:27:01 -0400

    Move episodes/showNotes fixtures into utils

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import episodes from 'ember-weekend/models/episodes';
-import showNotes from 'ember-weekend/models/show-notes';
+import episodes from 'ember-weekend/utils/prod-fixtures/episodes';
+import showNotes from 'ember-weekend/utils/prod-fixtures/show-notes';
 
 export default function(server) {
   episodes.forEach((e) => {
