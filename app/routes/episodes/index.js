e1921c2a71882f0b6d94915af88af11cb5ca7845
Joshua Plicque and Jonathan Jackson <dev+plicjo+rondale-sc@hashrocket.com>
2015-10-21 15:40:57 -0400

    Ensure findAll isn't cached

--- app/routes/episodes/index.js
+++ app/routes/episodes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('episode').then((episodes) => {
+    return this.get('store').findAll('episode', { reload: true }).then((episodes) => {
       return episodes.sortBy('number').reverse();
     });
   }
