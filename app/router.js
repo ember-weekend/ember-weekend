eba86213a8bfa61871baafbe98bd75a6256e6e3d
Jonathan Jackson <jonathan.jackson1@me.com>
2015-10-23 14:50:05 -0400

    Remove anon func on 'blog' route

--- app/router.js
+++ app/router.js
@@ -15,7 +15,7 @@ Router.map(function() {
     this.route('episodes', { resetNamespace: true }, function() {
       this.route('show', { path: '/:slug' });
     });
-    this.route('blog', function() {});
+    this.route('blog');
     this.route('blog.show', { path: 'blog/:permalink' });
     this.route('about');
   });
