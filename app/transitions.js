725c5d23995ae2bed15048ebaeb8d39db282f66e
Jonathan Jackson and Rye Mason <dev+rondale-sc+ryenotbread@hashrocket.com>
2015-10-23 16:32:58 -0400

    Add transition and blog nav styles

--- app/transitions.js
+++ app/transitions.js
@@ -9,4 +9,17 @@ export default function() {
     this.use('toUp'),
     this.reverse('toDown')
   );
+
+  this.transition(
+    this.toRoute('root.blog'),
+    this.use('toDown'),
+    this.reverse('toUp')
+  );
+
+  this.transition(
+    this.fromRoute('root.blog'),
+    this.toRoute('root.blog.show'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
 }
