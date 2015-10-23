fcfc03666bf72a304be4d963478a922e568ffc8a
Jonathan Jackson <jonathan.jackson1@me.com>
2015-10-23 16:09:34 -0400

    [FIX] directly link to episode route instead of relying on replaceState
    /ht @joshuac for pointing this bug out to us.  <3

--- app/routes/root/index.js
+++ app/routes/root/index.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  player: Ember.inject.service(),
-  init() {
-    this._super.apply(this, arguments);
-    const mostRecent = this.store.peekAll('episode').sortBy('releaseDate').get('lastObject');
-    this.set('player.episode', mostRecent);
-  },
-  beforeModel() {
+  beforeModel(transition) {
     this.replaceWith('episodes');
   }
 });
