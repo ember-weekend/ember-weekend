cd54a781c73e921cdcbf3111015926e33b0a5284
Joshua Plicque and Jonathan Jackson <dev+plicjo+rondale-sc@hashrocket.com>
2015-11-09 21:13:00 -0500

    Ensure external links are listed as nofollow

--- app/components/link-to-site.js
+++ app/components/link-to-site.js
@@ -4,7 +4,8 @@ import layout from '../templates/components/link-to-site';
 export default Ember.Component.extend({
   layout,
   tagName: 'a',
-  attributeBindings: ['href', 'target'],
+  attributeBindings: ['href', 'target', 'rel'],
+  rel: 'nofollow',
   target: Ember.computed(function() {
     return '_blank';
   })
