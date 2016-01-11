cd947481c07e5954883b34fb850db75c58c1a469
Chase McCarthy <dev+code0100fun@hashrocket.com>
2016-01-11 12:12:16 -0500

    Update ember-cli-sass and fix errors

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     sassOptions: {
-      ext: 'sass'
+      extension: 'sass'
     }
   });
 
