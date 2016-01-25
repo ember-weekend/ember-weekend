eaa4c4394ee84702c03be331668d133e9db211b3
Chase McCarthy <dev+code0100fun@hashrocket.com>
2016-01-24 23:41:16 -0500

    Switch to Pagefront for deployments

--- config/deploy.js
+++ config/deploy.js
@@ -1,29 +1,8 @@
-/* jshint node: true */
-
 module.exports = function(deployTarget) {
-  var ENV = {
-    build: {}
+  return {
+    pagefront: {
+      app: 'ember-weekend',
+      key: process.env.PAGEFRONT_KEY
+    }
   };
-
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    ENV.pipeline = {
-      activateOnDeploy: true
-    };
-    ENV.s3 = {
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
-      bucket: 'emberweekend-site',
-      region: 'us-east-1'
-    };
-    ENV['s3-index'] = {
-      allowOverwrite: true,
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
-      bucket: 'emberweekend-site',
-      region: 'us-east-1'
-    };
-  }
-
-  return ENV;
 };
