7ea76e685144566e142e320bfd23921e3d125162
Jonathan Jackson <dev+rondale-sc@hashrocket.com>
2016-04-25 16:19:50 -0400

    Temporarily disable fingerprinting ¯\_(ツ)_/¯

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    fingerprint: {
+      enabled: false
+    },
     sassOptions: {
       extension: 'sass'
     }
