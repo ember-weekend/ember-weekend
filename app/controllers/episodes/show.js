0e9593b511ec7d47f419552eec2dd13f2dbc86a1
Jonathan Jackson <dev+rondale-sc@hashrocket.com>
2016-04-29 13:45:58 -0400

    Remove keyboard acceptance test until mousetrap is fastboot compliant

--- app/controllers/episodes/show.js
+++ app/controllers/episodes/show.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   episode: Ember.computed.alias('model'),
-  showNotes: Ember.computed('episode.showNotes.length', function(){
+  showNotes: Ember.computed('episode.showNotes.length', function() {
     return this.get('episode.showNotes').sortBy('timeStamp');
   })
 });
--- app/utils/truncate-string.js
+++ app/utils/truncate-string.js
@@ -1,6 +1,6 @@
-export default function (str, length) {
+export default function(str, length) {
   if (str.length <= length) {
     return str.toString();
   }
-  return str.slice(0, length) + '...';
+  return `${str.slice(0, length)}...`;
 }
