1e4b48d1a72fb5e178ec110e580e4b7c3a64b0a6
Chase McCarthy <dev+code0100fun@hashrocket.com>
2017-03-22 11:19:14 -0500

    Specify node-sass version in package.json

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,6 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var nodeSass = require('node-sass');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -7,7 +8,8 @@ module.exports = function(defaults) {
       enabled: false
     },
     sassOptions: {
-      extension: 'sass'
+      extension: 'sass',
+      nodeSass: nodeSass
     }
   });
 
