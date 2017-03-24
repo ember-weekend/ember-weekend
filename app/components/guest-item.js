d5d2cb337cc30768cc61b1a8971c976fdd0e19d6
Chase McCarthy <dev+code0100fun@hashrocket.com>
2017-03-24 01:53:14 -0500

    Fix guest avatar image URL

--- app/components/guest-item.js
+++ app/components/guest-item.js
@@ -3,7 +3,4 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['guest'],
-  correctedAvatarURL: Ember.computed('avatarURL', function() {
-    return '/' + this.get('avatarURL');
-  })
 });
