024126265814315f3cf524812ab582116497a763
Chase McCarthy <dev+code0100fun@hashrocket.com>
2017-04-24 23:59:51 -0500

    Use ember-data-fastboot to populate shoebox

--- app/routes/episodes/index.js
+++ app/routes/episodes/index.js
@@ -4,38 +4,10 @@ const { set } = Ember;
 export default Ember.Route.extend({
   player: Ember.inject.service(),
   headData: Ember.inject.service(),
-  fastboot: Ember.inject.service(),
-  shortcircuit: Ember.inject.service('shoebox-shortcircuit'),
   model() {
-    let shoebox = this.get('fastboot.shoebox');
-    let shoeboxStore = shoebox.retrieve('episode-index-store');
-
-    if (this.get('fastboot.isFastBoot')) {
-
-      return this.store.findAll('episode').then(episodes => {
-        if (!shoeboxStore){
-          const pushData = episodes.map(episode => {
-            return episode.serialize({includeId: true});
-          });
-          shoebox.put('episode-index-store', pushData);
-        }
-
-        return episodes.sortBy('releaseDate').reverse();
-      });
-
-    } else {
-      if (shoeboxStore && typeof this.get('shortcircuit.episode-index-store') === 'undefined') {
-        shoeboxStore.forEach(episode => { this.store.pushPayload(episode); });
-        this.get('shortcircuit').set("episode-index-store", true);
-        return Ember.RSVP.resolve(
-          this.store.peekAll('episode').sortBy('releaseDate').reverse()
-        );
-      } else {
-        return this.store.findAll('episode', { reload: true}).then(episodes => {
-          return episodes.sortBy('releaseDate').reverse();
-        });
-      }
-    }
+    return this.store.findAll('episode', { reload: true}).then(episodes => {
+      return episodes.sortBy('releaseDate').reverse();
+    });
   },
   afterModel(episodes){
     set(this, 'headData.title', 'Episodes');
--- app/routes/episodes/show.js
+++ app/routes/episodes/show.js
@@ -5,50 +5,9 @@ const { set } = Ember;
 
 export default Ember.Route.extend({
   fastboot: Ember.inject.service(),
-  shortcircuit: Ember.inject.service('shoebox-shortcircuit'),
   player: Ember.inject.service(),
   model(params) {
-    let shoebox = this.get('fastboot.shoebox');
-    let shoeboxStore = shoebox.retrieve('episode-show-store');
-
-    if (this.get('fastboot.isFastBoot')) {
-      return this.store.queryRecord('episode', { slug: params.slug }).then(episode => {
-        if(!shoeboxStore){
-          shoeboxStore = {};
-          shoebox.put('episode-show-store', shoeboxStore);
-        }
-        const pushData = episode.serialize({includeId: true});
-
-        const includeds = [];
-
-        episode.get('showNotes').map(function(showNote) {
-
-          includeds.push(showNote.serialize({includeId: true}).data);
-
-          const resource = showNote.get('resource');
-          if (resource) {
-            includeds.push(resource.serialize({includeId: true}).data);
-            resource.get('authors').map(function(author) {
-              includeds.push(author.serialize({includeId: true}).data);
-            });
-          }
-        });
-
-        pushData.included = includeds;
-        shoeboxStore[params.slug] = pushData;
-        return episode;
-      });
-    } else {
-      if (shoeboxStore && typeof this.get('shortcircuit.episode-show-store') === 'undefined') {
-        let pushData;
-        pushData = shoeboxStore[params.slug];
-        this.store.pushPayload(pushData);
-        this.get('shortcircuit').set("episode-show-store", true);
-        return this.store.peekRecord('episode', pushData.data.id);
-      } else {
-        return this.store.queryRecord('episode', { slug: params.slug });
-      }
-    }
+    return this.store.queryRecord('episode', { slug: params.slug });
   },
   serialize(model) {
     return { slug: model.get('slug') };
