f53d5a3b662bb16dfab4d717284cf17f0133a9c7
Chase McCarthy <dev+code0100fun@hashrocket.com>
2017-04-24 19:37:24 -0500

    Use show note note field if there is no resource title

--- app/models/show-note.js
+++ app/models/show-note.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+
+const { computed } = Ember;
 const { attr } = DS;
 
 export default DS.Model.extend({
-  timeStamp: attr(),
+  timeStamp: attr('string'),
+  note: attr('string'),
+  title: computed('resource.title', 'note', function() {
+    return this.get('resource.title') || this.get('note');
+  }),
   resource: DS.belongsTo('resource', { async: false }),
   authors: Ember.computed('resource', function() {
     return this.get('resource.authors');
-  })
+  }),
 });
