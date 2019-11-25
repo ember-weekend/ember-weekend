99e7b230cd71347207471ef85292744dbeb38cdc
Chase McCarthy <chase@code0100fun.com>
2019-11-25 00:46:18 -0600

    Whitelist classes used in component *.js files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,6 +12,14 @@ const purgeCSS = {
       './app/index.html',
       './app/templates/**/*.hbs'
     ],
+    whitelist: [ // classes used in component *.js files. Remove once using Glimmer components.
+      'w-full', 'sm:w-1/5', 'h-auto', 'sm:h-full', 'flex', 'bg-gray-150', 'border-r', 'border-gray-400',
+      'h-full', 'flex', 'flex-col', 'sm:flex-row',
+      'bg-white', 'border-b', 'border-gray-400', 'flex', 'flex-col', 'sm:flex-row', 'p-8', 'items-center',
+      'p-6', 'bg-white', 'border-b', 'border-gray-400', 'flex', 'flex-row', 'items-start',
+      'flex-1', 'overflow-auto',
+      'text-white', 'text-center', 'text-sm', 'bg-orange-300', 'hover:bg-orange-200', 'w-16', 'h-10', 'p-2', 'rounded-lg',
+    ],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
   }
 }
