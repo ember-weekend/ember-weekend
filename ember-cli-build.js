4801bf37a98c367c0e954179eb9c5bca7060d35d
Chase McCarthy <chase@code0100fun.com>
2019-11-24 23:59:08 -0600

    Enable Purge CSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,17 +4,17 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const fetchRoutes = require('./bin/fetch-routes');
 const isProduction = EmberApp.env() === 'production';
 
-// const purgeCSS = {
-//   module: require('@fullhuman/postcss-purgecss'),
-//   options: {
-//     content: [
-//       // add extra paths here for components/controllers which include tailwind classes
-//       './app/index.html',
-//       './app/templates/**/*.hbs'
-//     ],
-//     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-//   }
-// }
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -31,7 +31,7 @@ module.exports = function(defaults) {
             }
           },
           require('tailwindcss')('./app/tailwind/config.js'),
-          ...isProduction ? [/*purgeCSS*/] : []
+          ...isProduction ? [purgeCSS] : []
         ]
       }
     }
